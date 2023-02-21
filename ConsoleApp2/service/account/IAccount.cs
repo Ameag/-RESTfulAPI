ba282f0fb2ea79ed3a449b1ebb1b0fc097b15bc8
Ameag <nekqga@gmail.com>
2023-02-21 21:19:26 +0500

    Реализует service и handler

---
+++ ConsoleApp2/service/account/IAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace It-planetaApi.Net.service.account
+{
+    internal interface IAccount
+{
+}
+}
